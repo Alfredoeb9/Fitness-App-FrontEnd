d48ca6f142c72eaca233a798d933ee250f101a1e
Alfredoeb9 <alfredoeb96@gmail.com>
2023-02-14 11:58:05 -0800

    [routes] fixed redux routes based on new updates from redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { login } from "./app/features/AuthContext";
+import RootLayout from "./layouts/RootLayout";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
@@ -13,6 +19,21 @@ import "./App.css";
 import PrivateRoutes from "./components/PrivateRoutes";
 import Welcome from "./pages/Welcome";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route element={<PrivateRoutes />}>
+        <Route index element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+      <Route path="/welcome" element={<Welcome />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
 function App() {
   const dispatch = useDispatch();
 
@@ -23,25 +44,7 @@ function App() {
       dispatch(login(user));
     }
   }, [dispatch]);
-  return (
-    <div className="app">
-      <BrowserRouter>
-        <Navbar />
-        <div className="app__pages">
-          <Routes>
-            <Route element={<PrivateRoutes />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/profile" element={<Profile />} />
-            </Route>
-            <Route path="/welcome" element={<Welcome />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
