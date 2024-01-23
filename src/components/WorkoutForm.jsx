b25ce7ad4428690d3271355117c6450bd867d247
Alfredoeb9 <alfredoeb96@gmail.com>
2024-01-23 14:51:08 -0800

    [toast] added toast module

--- src/components/WorkoutForm.jsx
+++ src/components/WorkoutForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
 import { useDispatch, useSelector } from "react-redux";
 import Switch from "@mui/material/Switch";
 import { createWorkout } from "../app/features/workoutSlice";
