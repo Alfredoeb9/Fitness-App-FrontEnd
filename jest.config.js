67756ee49224d399f719df8cef5db3addf5dd7fc
Alfredoeb9 <alfredoeb96@gmail.com>
2024-01-25 09:06:45 -0800

    [clean up] cleaned up code

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    
-    // preset: 'ts-jest',
-    transform: {
-      // '^.+\\.(ts|tsx)?$': 'ts-jest',
-      '^.+\\.(js|jsx)$': 'babel-jest',
-    },
-    moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-          '<rootDir>/src/__mock__/fileMock.js',
-        '\\.(css|less)$': '<rootDir>/test/__mock__/styleMock.js',
-    },
-    "testEnvironment": "<rootDir>/test/test-config/env-setup.js",
-    "setupFilesAfterEnv": ["<rootDir>/test/test-config/dom-setup.js"]
+  // preset: 'ts-jest',
+  transform: {
+    // '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        '<rootDir>/src/__mock__/fileMock.js',
+      '\\.(css|less)$': '<rootDir>/test/__mock__/styleMock.js',
+  },
+  "testEnvironment": "<rootDir>/test/test-config/env-setup.js",
+  "setupFilesAfterEnv": ["<rootDir>/test/test-config/dom-setup.js"]
 };
--- test/test-config/dom-setup.js
+++ test/test-config/dom-setup.js
@@ -1,19 +1,3 @@
-import '@testing-library/jest-dom';
-import { TextEncoder } from 'util';
-
-const Environment = require("jest-environment-jsdom").default;
-
-module.exports = class CustomTestEnvironment extends Environment {
-    async setup() {
-        await super.setup();
-        this.global.TextEncoder = TextEncoder;
-        this.global.TextDecoder = TextDecoder;
-        this.global.Response = Response;
-        this.global.Request = Request;
-        
-    }
-};
-
 const { JSDOM } = require("jsdom");
 
 const DEFAULT_HTML = `<html>
