4b53a2f6bce0fe76f018e904ec91b62e2ebe7ad0
Alfredoeb9 <alfredoeb96@gmail.com>
2024-10-02 18:59:25 -0700

    [unit test] updated unit test packages and config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   preset: "ts-jest",
   transform: {
-    // '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(ts|tsx)?$": "ts-jest",
     "^.+\\.(js|jsx)$": "babel-jest",
   },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/src/__mock__/fileMock.ts",
+      "<rootDir>/test/__mock__/fileMock.ts",
     "\\.(css|less)$": "<rootDir>/test/__mock__/styleMock.ts",
   },
   testEnvironment: "<rootDir>/test/test-config/env-setup.js",
--- test/test-config/dom-setup.js
+++ test/test-config/dom-setup.js
@@ -1,23 +1,22 @@
 const { JSDOM } = require("jsdom");
 
-const DEFAULT_HTML = `<html>
+const DEFAULT_HTML = `<!DOCTYPEhtml>
     <head><title>A1 Fitness Logger</title></head>
 </html>`;
 
 const DOM = new JSDOM(DEFAULT_HTML, {
-    url: 'http://localhost:3002',
+  url: "http://localhost:3002",
 });
 const { window } = DOM;
-
 
 global.window = window;
 global.document = window.document;
 global.location = window.location;
 global.navigator = {
-    userAgent: 'node.js',
-    platform: 'windows',
+  userAgent: "node.js",
+  platform: "windows",
 };
-global.sessionStorage= window.sessionStorage;
+global.sessionStorage = window.sessionStorage;
 
 global.TextEncoder = TextEncoder;
 global.MutationObserver = window.MutationObserver;
