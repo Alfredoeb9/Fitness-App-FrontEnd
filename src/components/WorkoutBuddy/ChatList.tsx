0e4ccee2b67f1bbed2d1aaf6cf2bd974199492e1
Alfredoeb9 <alfredoeb96@gmail.com>
2025-06-28 13:58:58 -0700

    [delete] create delete UI function

--- src/components/WorkoutBuddy/ChatList.tsx
+++ src/components/WorkoutBuddy/ChatList.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import { useAppSelector } from "../../app/hooks";
 import { selectUserAuth } from "../../app/features/AuthContext";
+import { TrashIcon } from "../../assets/svg/TrashIcon"
 import "./chatList.css";
 
 export default function ChatList() {
   const user = useAppSelector(selectUserAuth);
+  const queryClient = useQueryClient(); // Add this hook
 
   const { isLoading, data, error } = useQuery(["userChats"], {
     queryFn: async () => {
@@ -31,6 +33,30 @@ export default function ChatList() {
     enabled: !!user,
   });
 
+  const handleDeleteChat = async (chatId: string) => {
+    if (window.confirm("Are you sure you want to delete this chat?")) {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/api/workout-buddy/deletechat/${chatId}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${user.token}`,
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to delete chat");
+        }
+
+        await queryClient.invalidateQueries(["userChats"]);
+      } catch (error) {
+        console.error("Error deleting chat:", error);
+      }
+    }
+  };
+
   return (
     <div className="workout_buddy-nav">
       <span className="workout_buddy-title">DASHBOARD</span>
@@ -44,9 +70,18 @@ export default function ChatList() {
         ) : (
           <>
             {data?.chats?.map((chat: { _id: React.Key; title: string }) => (
-              <Link className="workout_buddy-link" key={chat._id} to={`/workout-buddy/chat/${chat._id}`}>
-                {chat.title}
-              </Link>
+
+              <div className="workout_buddy-chat" key={chat._id}>
+                <Link className="workout_buddy-link" to={`/workout-buddy/chat/${chat._id}`}>
+                  {chat.title}
+                </Link>
+
+                <TrashIcon
+                  className="workout_buddy-delete"
+                  onClick={() => handleDeleteChat(String(chat._id))}
+                />
+              </div>
+              
             ))}
           </>
         )}
