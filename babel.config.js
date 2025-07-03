0acf569d7cb9f06c1044870095c3539cb79bdf29
Alfredoeb9 <alfredoeb96@gmail.com>
2025-07-02 22:11:08 -0700

    [babel] updated babel
    applying correct typescript files during build process

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,23 @@
 module.exports = (/** @type {{ cache: (arg0: boolean) => void; }} */ api) => {
     api.cache(false)
     const presets = [
-      ['@babel/preset-typescript'],
-      ['@babel/preset-react', {runtime: 'automatic'}],
-      [
-        '@babel/preset-env',
-        {
-          corejs: { version: 3 },
-          useBuiltIns: 'usage',
-          // modules: false,
-          targets: {
-            edge: '17',
-            firefox: '60',
-            chrome: '67',
-            safari: '11.1',
-            ie: '11',
-          },
+      ['@babel/preset-env', {
+        corejs: { version: 3 },
+        useBuiltIns: 'usage',
+        targets: {
+          edge: '17',
+          firefox: '60',
+          chrome: '67',
+          safari: '11.1',
         },
-      ],
+      }],
+      ['@babel/preset-react', { 
+        runtime: 'automatic' 
+      }],
+      ['@babel/preset-typescript', {
+        isTSX: true,
+        allExtensions: true,
+      }],
     ]
     const plugins = [
       ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
